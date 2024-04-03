61e76745141cacbdf445a8daa12bf90e8c77d996
Agilan <agilandeena41@gmail.com>
2024-04-04 00:53:05 +0530

    second one

--- Application/MicrofrontendswithReactACompleteDevelopersGuide/container/webpack.config.js
+++ Application/MicrofrontendswithReactACompleteDevelopersGuide/container/webpack.config.js
@@ -1,11 +1,19 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
   devServer: {
-    port: 8082,
+    port: 4000,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:4001/remoteEntry.js",
+        cart: "cart@http://localhost:4002/remoteEntry.js",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
